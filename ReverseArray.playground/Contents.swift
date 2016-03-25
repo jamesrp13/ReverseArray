7a812f4be9d20bdf32741ac63972fe19caa2ae0f
Adam Aldous <aldousadam@gmail.com>
2016-03-25 09:08:08 -0600

    Done

--- ReverseArray.playground/Contents.swift
+++ ReverseArray.playground/Contents.swift
@@ -3,3 +3,15 @@
 import UIKit
 
 // Create a function that takes an array of integers and prints the array in reverse
+
+func reverseArray(input: [Int]) {
+    
+    var newArray: [Int] = []
+    
+    for num in input {
+        newArray.insert(num, atIndex: 0)
+    }
+    print(newArray)
+}
+
+reverseArray([1,2,3,4,5,6])
