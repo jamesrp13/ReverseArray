faa0153e1ae22fe23f56c5af39bf2bdd4f3744e8
retika <retika@retikas-macbook-pro.local>
2016-03-25 09:08:43 -0600

    final array

--- ReverseArray.playground/Contents.swift
+++ ReverseArray.playground/Contents.swift
@@ -2,4 +2,23 @@
 
 import UIKit
 
-// Create a function that takes an array of integers and prints the array in reverse
+// Create a function that takes an array of integers and prints the array in reversefunc reverseArray(array: [Int]) -> [Int] {
+
+func reverseArray(array: [Int]) -> [Int] {
+    var newArray: [Int] = []
+    for (var i = array.count - 1; i >= 0; i--) {
+        newArray.append(array[i])
+        
+    }
+    return newArray
+}
+reverseArray([2, 4, 43, 99])
+
+func anotherReverseArray(array: [Int]) -> [Int] {
+    var newArray: [Int] = []
+    for number in array {
+        newArray.insert(number, atIndex: 0)
+    }
+    return newArray
+}
+anotherReverseArray([99, 38, 8])
